1bae10c63e6525fb985b1a8d3cba284b4b6e50c2
mwatkin8 <michael.watkins.8@gmail.com>
2020-06-04 15:21:32 -0600

    all data in tables

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ async function getPatients(){
 
 async function buildSelect(patients){
   let li = d3.select('#patient');
-  let select = li.append('select').attr('id','select');
+  let select = li.append('select').attr('id','select').attr('onchange','changeSelection()');
   patients.forEach((p, i) => {
     if(i === 0){
         select.append('option').attr('value', p).attr('selected','selected').text(p);
@@ -38,17 +38,26 @@ async function buildSelect(patients){
   });
 }
 
-async function init(){
-  getPatients()
-  //demographics()
-  //console.log(subjects);
+async function changeSelection(){
+  d3.selectAll('.med-container').remove();
+  let selected = d3.select("#select").node().value;
+  demographics(selected);
+  let meds = await getMeds(selected);
+  buildTable(meds);
 }
 
+async function init(){
+  await getPatients()
+  let selected = d3.select("#select").node().value;
+  demographics(selected);
+  let meds = await getMeds(selected);
+  buildTable(meds);
+}
 
-async function demographics(){
-    let response = await fetch(server + '/Patient?_id=' + patient);
-    let bundle = await response.json();
-    let p = bundle.entry[0].resource;
+async function demographics(selected){
+    let url = server + '/' + selected;
+    let response = await fetch(url);
+    let p = await response.json();
     let name = p.name[0].given[0] + ' ' + p.name[0].family + ' ';
     d3.select('#name').text(name);
     let today = new Date();
@@ -57,27 +66,36 @@ async function demographics(){
     d3.select('#dems').text(gender + ', ' + age + ' years old');
 }
 
-async function medCodes(){
+async function getMeds(selected){
     //Get MedicationRequest resources for patient and extract RxNorm codes
-    let bundle = await getResource(server + '/MedicationRequest?subject=SMART-1288992');
+    let id = selected.split('/')[1];
+    let response = await fetch(server + '/MedicationRequest?subject=' + id);
+    let bundle = await response.json();
     let meds = [];
     bundle.entry.forEach((entry, i) => {
-      meds.push(entry.resource.medicationCodeableConcept.coding[0].code)
+      let code = entry.resource.medicationCodeableConcept.coding[0].code;
+      let display = entry.resource.medicationCodeableConcept.coding[0].display;
+      meds.push([code,display]);
     });
     return meds;
 }
 
 async function buildTable(meds){
+  let ref = 'https://davinci-drug-formulary-ri.logicahealth.org/fhir';
   let main = d3.select('#main');
-    meds.forEach(async (code, i) => {
-      let ref = 'https://davinci-drug-formulary-ri.logicahealth.org/fhir'
-      let response = await fetch(ref + '/MedicationKnowledge?code=' + code)
+    meds.forEach(async (m, i) => {
+      let code = m[0];
+      let display = m[1];
+      let container = main.append('div').attr('class','med-container');
+      container.append('h3').text(display);
+      //Query formularies for the medication
+      let response = await fetch(ref + '/MedicationKnowledge?code=' + code);
       let bundle = await response.json();
       if (bundle.total !== 0){
-        console.log(code);
-        main.append('h3').text(bundle.entry[0].resource.code.coding[0].display);
-        let div = main.append('div').attr('class','table-responsive med-table');
+        //If any exist, create a table to display the data
+        let div = container.append('div').attr('class','table-responsive med-table');
         let table = div.append('table').attr('class','table table-striped table-sm');
+        //Set headers for the table
         let head = table.append('thead');
         let row = head.append('tr');
         row.append('th').text('FormularyDrug');
@@ -86,10 +104,11 @@ async function buildTable(meds){
         row.append('th').text('StepTherapyLimit');
         row.append('th').text('QuantityLimit');
         row.append('th').text('PlanID');
+        //Create the table body
         let body = table.append('tbody');
-        //Loop through formularies for this medication
-        let planIDs = [];
+        let planIDs = []; //Keep a collection of CoveragePlan IDs for a separate table
         bundle.entry.forEach((entry, i) => {
+          //Create table rows for the related formularities
           row = body.append('tr');
           row.append('td').text(entry.fullUrl);
           entry.resource.extension.forEach((ext, i) => {
@@ -105,72 +124,74 @@ async function buildTable(meds){
             }
           });
         });
-        //Pull the CoveragePlan for each PlanID
-        table = div.append('table').attr('class','table table-striped table-sm');
-        head = table.append('thead');
-        row = head.append('tr');
-        row.append('th').text('CoveragePlan');
-        row.append('th').text('DrugTierID');
-        row.append('th').text('MailOrder');
-        row.append('th').text('CostSharing1');
-        row.append('th').text('CostSharing2');
-        row.append('th').text('CostSharing3');
-        row.append('th').text('CostSharing4');
-        body = table.append('tbody');
         planIDs.forEach(async (id, i) => {
           let response = await fetch(ref + '/List?identifier=' + id)
           let bundle = await response.json();
-          row = body.append('tr');
-          row.append('td').text(id);
-          bundle.entry[0].resource.extension.forEach((ext, i) => {
-            if(i === 0){
-              row.append('td').text(ext.valueCodeableConcept.coding[0].display);
-            }
-            else if (i === 1) {
-                row.append('td').text(ext.valueBoolean);
-            }
-            else{
-              row.append('td').text(JSON.stringify(ext));
+          div.append('h5').style('font-style','italic').html(
+            'CoveragePlan: ' + bundle.entry[0].resource.title
+          );
+          div.append('p').html(
+            'PlanID: ' + id +
+            '<br>PlanIDType: ' + bundle.entry[0].resource.extension[9].valueString+
+            '<br>Network: ' + bundle.entry[0].resource.extension[6].valueString + 
+            '<br>SummaryURL: ' + bundle.entry[0].resource.extension[7].valueString +
+            '<br>EmailPlanContact: ' + bundle.entry[0].resource.extension[8].valueString
+          )
+          //Create another table for the Coverage Plans referenced in the above formularies
+          table = div.append('table').attr('class','table table-striped table-sm');
+          //Set headers for the table
+          head = table.append('thead');
+          row = head.append('tr');
+          row.append('th').text('DrugTierID');
+          row.append('th').text('MailOrder');
+          row.append('th').text('CostSharing1');
+          row.append('th').text('CostSharing2');
+          row.append('th').text('CostSharing3');
+          row.append('th').text('CostSharing4');
+          //Table body
+          body = table.append('tbody');    
+          //10 fields      
+          bundle.entry[0].resource.extension.forEach((ext,i) => {
+            row = body.append('tr');
+            if(i < 6){
+              ext.extension.forEach((e,i) => {
+              if(i === 0){
+                row.append('td').text(e.valueCodeableConcept.coding[0].display);
+              }
+              else if (i === 1) {
+                  row.append('td').text(e.valueBoolean);
+              }
+              else{
+                let summary = '';
+                e.extension.forEach((costsharing, i) => {
+                  if(i === 0){
+                    summary += costsharing.url + ": " + costsharing.valueCodeableConcept.coding[0].display + '<br>';
+                  }
+                  if(i === 1){
+                    summary += costsharing.url + ": $" + costsharing.valueMoney.value + '<br>';
+                  }
+                  if(i === 2){
+                    summary += costsharing.url + ": $" + costsharing.valueDecimal + '<br>';
+                  }
+                  if(i === 3){
+                    summary += costsharing.url + ": " + costsharing.valueCodeableConcept.coding[0].display + '<br>';
+                  }
+                  if(i === 4){
+                    summary += costsharing.url + ": " + costsharing.valueCodeableConcept.coding[0].display + '<br>';
+                  }
+                })
+                row.append('td').html(summary);
+              }
+              });
             }
           });
-
-
         });
 
 
       }
+      else{
+        container.append('h6').text('No formularies for this medication.');
+      }
+      
     });
 }
-
-
-
-/*
-<h2>Section title</h2>
-<div class="table-responsive">
-  <table class="table table-striped table-sm">
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>Header</th>
-        <th>Header</th>
-        <th>Header</th>
-        <th>Header</th>
-      </tr>
-    </thead>
-    <tbody>
-    <tr>
-      <td>1,001</td>
-      <td>Lorem</td>
-      <td>ipsum</td>
-      <td>dolor</td>
-      <td>sit</td>
-    </tr>
-    </tbody>
-  </table>
-</div>
-
-
-
-
-
-*/
